0143ec971986d2d7e55a57fa5010eb0c650ae10b
mituwebdvp <mehjabinmitu124@gmail.comm>
2024-12-04 20:09:02 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("viewall").onclick = function() {
+    document.getElementById("imageshown").style.display = "block";
+    document.getElementById("categories").style.display = "none";
+}
